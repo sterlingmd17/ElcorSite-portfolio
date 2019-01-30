1c588afa24088130f15eea583b13f98fe6cdbaec
Sterling Dunn <SterlingD@ElcorInc.net>
2019-01-30 15:48:18 -0800

    More progress, added other pages.

--- Main.py
+++ Main.py
@@ -1,10 +1,26 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from App import app
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/', methods=['GET'])
 def index():
 
     return render_template("index.html")
+
+
+@app.route('/reasons', methods=['GET'])
+def reasons():
+
+    return render_template("reasons.html")
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name=request.form()
+
+
+    return render_template("contact.html")
+
+
 
 
 if __name__ == '__main__':
