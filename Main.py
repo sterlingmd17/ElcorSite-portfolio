27f0b8140efbd1a825ee55375269d6b40057f801
Sterling Dunn <SterlingD@ElcorInc.net>
2019-02-22 12:09:53 -0800

    Main.py changes added requirements.txt

--- Main.py
+++ Main.py
@@ -1,8 +1,7 @@
 from flask import Flask, request, redirect, render_template, session, flash
-from functools import wraps
+from App import app, recaptcha
 import requests
 import json
-from App import app, recaptcha
 
 @app.route('/', methods=['GET'])
 def index():
