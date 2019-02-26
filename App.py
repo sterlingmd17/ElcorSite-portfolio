b7507b224c36c22da39bfa75feeb18240a8d2364
sterling <sterlingmd17@gmail.com>
2019-02-25 19:51:18 -0800

    repush

--- App.py
+++ App.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_recaptcha import ReCaptcha
 
-
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI']='mysql://elcor:elcor@localhost:3306/elcor'
@@ -12,3 +11,4 @@ app.config['RECAPTCHA_PRIVATE_KEY'] = '6LdsiZIUAAAAAN4LVoj1xQxyMmFyV_AA6NFwGh0B'
 app.secret_key= "elcor"
 recaptcha = ReCaptcha(app=app)
 db = SQLAlchemy(app)
+
