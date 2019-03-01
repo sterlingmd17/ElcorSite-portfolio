20d23ca20f0e89fd5f775ff71d5c9a6f329887b6
sterling <sterlingmd17@gmail.com>
2019-02-28 17:50:14 -0800

    changed location of source into main

--- Main.py
+++ Main.py
@@ -1,9 +1,18 @@
 from flask import Flask,request, redirect, render_template, session, flash
-from application import app
+from flask_sqlalchemy import SQLAlchemy
+from flask_recaptcha import ReCaptcha
 import requests
 import json
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI']='mysql://elcor:elcor@localhost:3306/elcor'
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['RECAPTCHA_PUBLIC_KEY'] = '6LdsiZIUAAAAAJybnDIelA5soDfns61EHnTrN2Ha'
+app.config['RECAPTCHA_PRIVATE_KEY'] = '6LdsiZIUAAAAAN4LVoj1xQxyMmFyV_AA6NFwGh0B'
+app.secret_key= "elcor"
+recaptcha = ReCaptcha(app=app)
+db = SQLAlchemy(app)
 
 
 @app.route('/', methods=['GET'])
